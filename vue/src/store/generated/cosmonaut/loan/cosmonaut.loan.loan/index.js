9096d9b27e6e9c30ce2c15e80eeac26fd3a97298
haiTran <transonhai0805@gmail.com>
2022-01-15 18:00:26 +0700

    change logic approve loan

--- vue/src/store/generated/cosmonaut/loan/cosmonaut.loan.loan/index.js
+++ vue/src/store/generated/cosmonaut/loan/cosmonaut.loan.loan/index.js
@@ -165,23 +165,6 @@ export default {
                 throw new SpVuexError('QueryClient:QueryLoanAll', 'API Node Unavailable. Could not perform query: ' + e.message);
             }
         },
-        async sendMsgApproveLoan({ rootGetters }, { value, fee = [], memo = '' }) {
-            try {
-                const txClient = await initTxClient(rootGetters);
-                const msg = await txClient.msgApproveLoan(value);
-                const result = await txClient.signAndBroadcast([msg], { fee: { amount: fee,
-                        gas: "200000" }, memo });
-                return result;
-            }
-            catch (e) {
-                if (e == MissingWalletError) {
-                    throw new SpVuexError('TxClient:MsgApproveLoan:Init', 'Could not initialize signing client. Wallet is required.');
-                }
-                else {
-                    throw new SpVuexError('TxClient:MsgApproveLoan:Send', 'Could not broadcast Tx: ' + e.message);
-                }
-            }
-        },
         async sendMsgRequestLoan({ rootGetters }, { value, fee = [], memo = '' }) {
             try {
                 const txClient = await initTxClient(rootGetters);
@@ -199,18 +182,20 @@ export default {
                 }
             }
         },
-        async MsgApproveLoan({ rootGetters }, { value }) {
+        async sendMsgApproveLoan({ rootGetters }, { value, fee = [], memo = '' }) {
             try {
                 const txClient = await initTxClient(rootGetters);
                 const msg = await txClient.msgApproveLoan(value);
-                return msg;
+                const result = await txClient.signAndBroadcast([msg], { fee: { amount: fee,
+                        gas: "200000" }, memo });
+                return result;
             }
             catch (e) {
                 if (e == MissingWalletError) {
                     throw new SpVuexError('TxClient:MsgApproveLoan:Init', 'Could not initialize signing client. Wallet is required.');
                 }
                 else {
-                    throw new SpVuexError('TxClient:MsgApproveLoan:Create', 'Could not create message: ' + e.message);
+                    throw new SpVuexError('TxClient:MsgApproveLoan:Send', 'Could not broadcast Tx: ' + e.message);
                 }
             }
         },
@@ -226,6 +211,21 @@ export default {
                 }
                 else {
                     throw new SpVuexError('TxClient:MsgRequestLoan:Create', 'Could not create message: ' + e.message);
+                }
+            }
+        },
+        async MsgApproveLoan({ rootGetters }, { value }) {
+            try {
+                const txClient = await initTxClient(rootGetters);
+                const msg = await txClient.msgApproveLoan(value);
+                return msg;
+            }
+            catch (e) {
+                if (e == MissingWalletError) {
+                    throw new SpVuexError('TxClient:MsgApproveLoan:Init', 'Could not initialize signing client. Wallet is required.');
+                }
+                else {
+                    throw new SpVuexError('TxClient:MsgApproveLoan:Create', 'Could not create message: ' + e.message);
                 }
             }
         },
--- vue/src/store/generated/cosmonaut/loan/cosmonaut.loan.loan/module/index.js
+++ vue/src/store/generated/cosmonaut/loan/cosmonaut.loan.loan/module/index.js
@@ -2,11 +2,11 @@
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
-import { MsgApproveLoan } from "./types/loan/tx";
 import { MsgRequestLoan } from "./types/loan/tx";
+import { MsgApproveLoan } from "./types/loan/tx";
 const types = [
-    ["/cosmonaut.loan.loan.MsgApproveLoan", MsgApproveLoan],
     ["/cosmonaut.loan.loan.MsgRequestLoan", MsgRequestLoan],
+    ["/cosmonaut.loan.loan.MsgApproveLoan", MsgApproveLoan],
 ];
 export const MissingWalletError = new Error("wallet is required");
 export const registry = new Registry(types);
@@ -27,8 +27,8 @@ const txClient = async (wallet, { addr: addr } = { addr: "http://localhost:26657
     const { address } = (await wallet.getAccounts())[0];
     return {
         signAndBroadcast: (msgs, { fee, memo } = { fee: defaultFee, memo: "" }) => client.signAndBroadcast(address, msgs, fee, memo),
-        msgApproveLoan: (data) => ({ typeUrl: "/cosmonaut.loan.loan.MsgApproveLoan", value: MsgApproveLoan.fromPartial(data) }),
         msgRequestLoan: (data) => ({ typeUrl: "/cosmonaut.loan.loan.MsgRequestLoan", value: MsgRequestLoan.fromPartial(data) }),
+        msgApproveLoan: (data) => ({ typeUrl: "/cosmonaut.loan.loan.MsgApproveLoan", value: MsgApproveLoan.fromPartial(data) }),
     };
 };
 const queryClient = async ({ addr: addr } = { addr: "http://localhost:1317" }) => {
